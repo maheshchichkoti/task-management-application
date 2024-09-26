7e7503c955167c87a559035510af63da172d4b14
Mahesh <107948691+maheshchichkoti@users.noreply.github.com>
2024-09-27 01:20:37 +0530

    Added frontend in the app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ const taskRoutes = require("./routes/taskRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 const mongoUrl = process.env.MONGODB_URL;
 
@@ -24,13 +28,6 @@ mongoose.connect(mongoUrl, (err) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
-  );
-}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
